92b293b0c8a10daf9492103460d374ac9afcbdf2
SolonetsMykhailo <solmisha2422@gmail.com>
2025-03-02 18:34:30 +0200

    task 2

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
-let product_name = "Генератор захисного поля";
-let price = 1000;
-console.log(`Обрано ${product_name}, ціна за штуку ${price} кредитів `);
-price = 2000;
-console.log(`Обрано ${product_name}, ціна за штуку ${price} кредитів `);
+// let product_name = "Генератор захисного поля";
+// let price = 1000;
+// console.log(`Обрано ${product_name}, ціна за штуку ${price} кредитів `);
+// price = 2000;
+// console.log(`Обрано ${product_name}, ціна за штуку ${price} кредитів `);
