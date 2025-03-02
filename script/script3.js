a90e125a7f1f3eef6c822b9670b8a31292ee6e33
SolonetsMykhailo <solmisha2422@gmail.com>
2025-03-02 19:47:49 +0200

    4 task

--- script/script3.js
+++ script/script3.js
@@ -1,13 +1,13 @@
-const ADMIN_PASSWORD = "jqueryismyjam";
-let message;
-const userInput = prompt("HASLO:");
-if (userInput === null){
-    message = "Скасовано користувачем";
-}
-else if (userInput === ADMIN_PASSWORD){
-    message = "Ласкаво просимо!";
-}
-else {
-    message = "Доступ заборонений, невіриний пароль";
-}
-alert(message);
+// const ADMIN_PASSWORD = "jqueryismyjam";
+// let message;
+// const userInput = prompt("HASLO:");
+// if (userInput === null){
+//     message = "Скасовано користувачем";
+// }
+// else if (userInput === ADMIN_PASSWORD){
+//     message = "Ласкаво просимо!";
+// }
+// else {
+//     message = "Доступ заборонений, невіриний пароль";
+// }
+// alert(message);
---
+++ script/script4.js
@@ -0,0 +1,17 @@
+let credits = 23580;
+const pricePerDroid = 3000;
+const input = prompt("Скільки дроїдів ви хочете придбати?");
+const numDroids = Number(input);
+    const totalPrice = numDroids * pricePerDroid;
+if (input === null){
+    console.log("Скасовано користувачем!");
+}
+else{
+    if (totalPrice > credits){
+        console.log("Недостатньо коштів на рахунку!");
+    }
+    else {
+        credits -= totalPrice;
+        console.log(`Ви купили ${numDroids} дроїдів, на рахунку залишилось ${credits} кредитів`)
+    }
+}
