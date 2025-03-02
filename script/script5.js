a46aa3f35944aa022b9c3987c2d74aad6783509e
SolonetsMykhailo <solmisha2422@gmail.com>
2025-03-02 21:35:16 +0200

    task 5

---
+++ script/script5.js
@@ -0,0 +1,38 @@
+const countryInput = prompt("Вкажіть країну доставки:");
+if (countryInput === null) {
+    console.log("Скасовано користувачем!");
+} 
+else {
+    const country = countryInput.trim().toLowerCase();
+    let price;
+    let delivercountry;
+    switch (country) {
+        case "китай":
+            price = 100;
+            delivercountry = "Китай";
+            break;
+        case "чилі":
+            price = 250;
+            delivercountry = "Чилі";
+            break;
+        case "австралія":
+            price = 170;
+            delivercountry = "Австралія";
+            break;
+        case "індія":
+            price = 80;
+            delivercountry = "Індія";
+            break;
+        case "ямайка":
+            price = 120;
+            delivercountry = "Ямайка";
+            break;
+        default:
+            alert("У вашій країні доставка недоступна");
+            price = null;
+            
+    }
+    if (price !== null) {
+        console.log(`Доставка в ${delivercountry} буде коштувати ${price} кредитів`);
+     }
+}
