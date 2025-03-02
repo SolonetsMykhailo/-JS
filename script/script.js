f84821e060a0d85f37b88373e56d7657b71bde87
SolonetsMykhailo <solmisha2422@gmail.com>
2025-03-02 18:12:52 +0200

    first task

--- script/script.js
+++ script/script.js
@@ -1,4 +1,5 @@
-function length(i) {
-    return i.length;
-}
-console.log(length("Hello"));
+let product_name = "Генератор захисного поля";
+let price = 1000;
+console.log(`Обрано ${product_name}, ціна за штуку ${price} кредитів `);
+price = 2000;
+console.log(`Обрано ${product_name}, ціна за штуку ${price} кредитів `);
