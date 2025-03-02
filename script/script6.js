edeeffb662900a7b269654089a345c81fa3f0d1e
SolonetsMykhailo <solmisha2422@gmail.com>
2025-03-02 22:05:42 +0200

    task 7

--- script/script6.js
+++ script/script6.js
@@ -1,11 +1,11 @@
-let input;
-let total = 0;
-while (true) {
-    input = prompt("Ваше число: ");
-    if (input === null){
-        break;
-    }
-    const number = Number(input);
-    total += number;
-}
-alert(`Загальна сума чисел дорівнює ${total}`);
+// let input;
+// let total = 0;
+// while (true) {
+//     input = prompt("Ваше число: ");
+//     if (input === null){
+//         break;
+//     }
+//     const number = Number(input);
+//     total += number;
+// }
+// alert(`Загальна сума чисел дорівнює ${total}`);
